42317af912c38d32ef3a166a7c8ad87a7a722f15
Eyroa <111017483+Eyroa@users.noreply.github.com>
2023-06-27 01:05:07 -0300

    Delete segurosAP directory

--- segurosAP/gestion_seguros/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Cliente, Poliza, Asegurado, Certificado
-
-# Register your models here.
-admin.site.register(Cliente)
-admin.site.register(Poliza)
-admin.site.register(Asegurado)
-admin.site.register(Certificado)
